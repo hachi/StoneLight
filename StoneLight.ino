37be3efac8ed4adf2c2ea689265a4d3511741ee9
Jonathan Steinert <hachi@kuiki.net>
2018-12-29 20:46:05 -0800

    Small adjustments

--- StoneLight.ino
+++ StoneLight.ino
@@ -7,15 +7,7 @@
 
 #define PIN 6
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(28, PIN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -25,24 +17,17 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);
 unsigned char mode = 0;
 
 void setup() {
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-#if defined (__AVR_ATtiny85__)
-  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-#endif
-  // End of trinket special code
-
-
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 
   EEPROM.get(0, mode);
-  EEPROM.update(0, (mode + 1) % 10);
+  EEPROM.update(0, (mode + 1) % 11);
 }
 
 void loop() {
   switch (mode) {
     case 0:
-      theaterChase(strip.Color(255, 63, 0), 50); // Red
+      theaterChase(strip.Color(255, 63, 0), 50); // Fire-ish
       break;
     case 1:
       colorWipe(strip.Color(255, 0, 0), 50); // Red
@@ -87,8 +72,6 @@ void loop() {
       break;
   }
   EEPROM.update(0, mode);
-  // Some example procedures showing how to display to the pixels:
-
 }
 
 // Fill the dots one after the other with a color
